2fd7b4a167e737ef6f33f8b034bf4a3f72fc489e
izzetomarovcff <izzetomarovcff@gmail.com>
2022-11-25 11:38:47 +0400

    end

--- function.js
+++ function.js
@@ -1,106 +1,144 @@
-const colr2 = document.querySelector(".two .curitem:nth-child(2)")
-const colr1 = document.querySelector(".one .curitem:first-child")
-colr1.style.backgroundColor = "#833AE0";
-colr2.style.backgroundColor = "#833AE0";
-const url = `https://api.exchangerate.host/latest`
-const costone = document.querySelector(".itemone .cost")
-const costtwo = document.querySelector(".itemtwo .cost")
-const valElments1 = document.querySelectorAll(".itemone .curitem")
-const valElments2 = document.querySelectorAll(".itemtwo .curitem")
-const inputOne = document.querySelector(".itemone .value")
-const inputTwo = document.querySelector(".itemtwo .value")
-const menuelement = document.querySelector(".mobmenubutton")
-const menumob = document.querySelector(".mobilemenu")
-const close = document.querySelector(".close")
-const selecet = (val) => {
-    val.forEach((item) => {
-        item.addEventListener("click", (e) => {
-            val.forEach((item) => {
-                item.style.background = "white"
-                item.style.color = "#C6C6C6"
-            })
-            item.style.background = "#833AE0"
-            item.style.color = "white"
-        })
+let inCur, outCur;
+const url = 'https://api.exchangerate.host/latest';
+const activeBtn = document.querySelectorAll('.btn-active');
+const btnIn = document.querySelectorAll('.input-select .btn-value');
+const btnOut = document.querySelectorAll('.output-select .btn-value');
+const curRateIn = document.querySelector('.input-area .currency-rate');
+const curRateOut = document.querySelector('.output-area .currency-rate');
+const inputIn = document.querySelector('.input-area .amount');
+const inputOut = document.querySelector('.output-area .amount');
+const menuBtn = document.querySelector('.hamburger');
+const menu = document.querySelector('.nav-content ul');
+const container = document.querySelector('.container');
+let curApiRateIn, curApiRateOut, space = 0;
+
+activeBtn.forEach((item, index) => {
+    if (index == 0)
+        inCur = item.value;
+    if (index == 1)
+        outCur = item.value;
+});
+
+
+const callApi = async (e) => {
+    window.addEventListener('offline', () => {
+        throw alert('No Internet Connection');
     })
-}
-selecet(valElments1)
-selecet(valElments2)
-const run = async (from, to, side) => {
-    if (side === "base") {
-        fetch(`${url}?base=${from}&symbols=${to}`)
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                inputTwo.value = inputOne.value * Object.values(data.rates)[0]
-            })
-    } else {
-        fetch(`${url}?base=${to}&symbols=${from}`)
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                inputOne.value = inputTwo.value * Object.values(data.rates)[0]
-            })
+    if (inCur == outCur) {
+        curApiRateIn = 1;
+        curApiRateOut = 1;
     }
-
-    fetch(`${url}?base=${from}&symbols=${to}`)
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            costone.innerText = `1 ${from} = ${Object.values(data.rates)[0]} ${to}`
-            inputOne.addEventListener("input", () => {
-                inputTwo.value = inputOne.value * Object.values(data.rates)[0]
-            })
-        })
-
-    fetch(`${url}?base=${to}&symbols=${from}`)
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            costtwo.innerText = `1 ${to} = ${Object.values(data.rates)[0]} ${from}`
-            inputTwo.addEventListener("input", () => {
-                inputOne.value = inputTwo.value * Object.values(data.rates)[0]
-            })
-        })
+    const resIn = await fetch(`${url}?base=${inCur}&symbols=${outCur}`);
+    const resOut = await fetch(`${url}?base=${outCur}&symbols=${inCur}`);
+    const dataIn = await resIn.json();
+    const dataOut = await resOut.json();
+    curApiRateIn = await Object.values(dataIn.rates)[0];
+    curApiRateOut = await Object.values(dataOut.rates)[0];
+    appendRate(e);
 }
-valElments1.forEach((item) => {
-    item.addEventListener("click", () => {
-        valElments2.forEach((element) => {
-            if (element.style.backgroundColor == "rgb(131, 58, 224)") {
-                intext1 = element.innerText
-            }
-        })
-        run(item.innerText, intext1, '')
-    })
-})
-valElments2.forEach((item) => {
-    item.addEventListener("click", () => {
-        valElments1.forEach((element) => {
-            if (element.style.backgroundColor == "rgb(131, 58, 224)") {
-                intext2 = element.innerText
-            }
-        })
-        run(intext2, item.innerText, "base")
-    })
-})
-menuelement.addEventListener("click", () => {
-    menumob.style.display = "flex"
-})
-close.addEventListener("click", () => {
-    menumob.style.display = "none"
-})
-function myFunction(x) {
-    if (x.matches) {
-    } else {
-        menumob.style.display = "none"
+
+callApi().catch(error => alert('something wrong'));
+
+function appendRate(e) {
+    curRateIn.textContent = `1 ${inCur} = ${curApiRateIn} ${outCur}`;
+    curRateOut.textContent = `1 ${outCur} = ${curApiRateOut} ${inCur}`;
+    if (e == 'output-select') {
+        if (inputOut.value != '') {
+            inputOut.value = +(inputIn.value.replaceAll(" ", "") * curApiRateIn).toFixed(6).substring(0, 13);
+        } else {
+            inputOut.value = '';
+        }
+    }
+    if (e == 'input-select') {
+        if (inputIn.value != '') {
+            inputIn.value = +(inputOut.value.replaceAll(" ", "") * curApiRateOut).toFixed(6).substring(0, 13);
+        } else {
+            inputIn.value = '';
+        }
     }
 }
-var x = window.matchMedia("(max-width: 810px)")
-myFunction(x)
-x.addListener(myFunction)
-run("RUB", "USD", "base")
-run("RUB", "USD", "")
+inputIn.addEventListener('keyup', (e) => {
+    if (e.target.value.length < 14) {
+        if (e.target.value == '') {
+            inputOut.value = "";
+        }
+        inputOut.value = +(e.target.value.replaceAll(" ", "") * curApiRateIn).toFixed(6).substring(0, 13);
+        inputOut.value = commify(inputOut.value);
+    }
+});
+
+inputOut.addEventListener('keyup', (e) => {
+    if (e.target.value.length < 14) {
+        if (e.target.value == '') {
+            inputIn.value = '';
+        }
+        inputIn.value = +(e.target.value.replaceAll(" ", "") * curApiRateOut).toFixed(6).substring(0, 13);
+        inputIn.value = commify(inputIn.value);
+    }
+});
+
+
+eventListener();
+
+function eventListener() {
+    btnIn.forEach(item => item.addEventListener('click', changeCurIn));
+    btnOut.forEach(item => item.addEventListener('click', changeCurOut));
+    menuBtn.addEventListener('click', openMenu);
+}
+
+function changeCurIn(e) {
+    const activeBtnIn = document.querySelectorAll('.input-select .btn-active');
+    activeBtnIn.forEach(item => item.classList.remove('btn-active'));
+    e.target.classList.add('btn-active');
+    inCur = e.target.value;
+    callApi(e.target.parentElement.classList[1]);
+}
+
+function changeCurOut(e) {
+    const activeBtnOut = document.querySelectorAll('.output-select .btn-active');
+    activeBtnOut.forEach(item => item.classList.remove('btn-active'))
+    e.target.classList.add('btn-active');
+    outCur = e.target.value;
+    callApi(e.target.parentElement.classList[1]);
+}
+
+function openMenu(e) {
+    e.target.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+}
+
+var numberMask = IMask(inputIn, {
+    mask: Number, // enable number mask
+    // other options are optional with defaults below
+    scale: 6, // digits after point, 0 for integers
+    signed: false, // disallow negative
+    thousandsSeparator: ' ', // any single char
+    padFractionalZeros: false, // if true, then pads zeros at end to the length of scale
+    normalizeZeros: true, // appends or removes zeros at ends
+    radix: '.', // fractional delimiter
+    mapToRadix: [','], // symbols to process as radix
+});
+
+var numberMask = IMask(inputOut, {
+    mask: Number, // enable number mask
+    // other options are optional with defaults below
+    scale: 6, // digits after point, 0 for integers
+    signed: false, // disallow negative
+    thousandsSeparator: ' ', // any single char
+    padFractionalZeros: false, // if true, then pads zeros at end to the length of scale
+    normalizeZeros: true, // appends or removes zeros at ends
+    radix: '.', // fractional delimiter
+    mapToRadix: [','], // symbols to process as radix
+});
+
+function commify(n) {
+    var parts = n.toString().split(".");
+    const numberPart = parts[0];
+    const decimalPart = parts[1];
+    const thousands = /\B(?=(\d{3})+(?!\d))/g;
+    return (
+        numberPart.replace(thousands, " ") + (decimalPart ? "." + decimalPart : "")
+    );
+}   
+inputIn.value = 0;
+inputOut.value = 0;
